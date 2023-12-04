79fa8585662662f50a9a639eb5e6efd6e7923963
Brian Vo <datvo2k@gmail.com>
2023-12-05 00:16:46 +0700

    change common-2

--- infrastructures/local/configs/join.sh
+++ infrastructures/local/configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.0.10:6443 --token b5qsrb.2d92u802x5cuu0yk --discovery-token-ca-cert-hash sha256:51cc9a16bf4dc89af9b65a46540f2c399fb2a9139cfe4e0cdef18edb03c2f515 
+kubeadm join 10.0.0.10:6443 --token ceh82o.dwdkviysoiibp70f --discovery-token-ca-cert-hash sha256:32571a2d8d5c00bea4261a4ed807169ccead9a2dd92151221c912e0ea8c5bdc8 
--- infrastructures/local/scripts/common-2.sh
+++ infrastructures/local/scripts/common-2.sh
@@ -20,104 +20,53 @@ sudo systemctl restart systemd-resolved
 # disable swap
 sudo swapoff -a
 
-# open port
-sudo ufw allow 6443
-sudo ufw allow 6784
-
 # keeps the swap off during reboot
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 sudo apt-get update -y
 
-# Brige mode
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
 EOF
- 
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat << EOF | tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
 sudo sysctl --system
 
-# install docker 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
+echo -e "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" >> /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get -y update
 
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo mkdir -p /etc/apt/keyrings
-# install kubeadm, kubectl, kubelet
+
+VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y containerd.io
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default > /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubectl kubeadm
-sudo apt-mark hold kubelet kubeadm kubectl
 sudo apt-get update -y
 sudo apt-get install -y jq
 
-sudo rm /etc/containerd/config.toml
-
-cat <<EOF | sudo tee /etc/containerd/config.toml
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-#disabled_plugins = ["cri"]
-#root = "/var/lib/containerd"
-#state = "/run/containerd"
-#subreaper = true
-#oom_score = 0
-
-#[grpc]
-#  address = "/run/containerd/containerd.sock"
-#  uid = 0
-#  gid = 0
-
-#[debug]
-#  address = "/run/containerd/debug.sock"
-#  uid = 0
-#  gid = 0
-#  level = "info"
-
-[plugins]
-  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-
-  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-      SystemdCgroup = true
-
-
-  [plugins."io.containerd.grpc.v1.cri"]
-  sandbox_image = "registry.k8s.io/pause:3.2"
-EOF
-
-sudo systemctl restart containerd
-sudo systemctl restart kubelet
-
 local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
-
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 ${ENVIRONMENT}
